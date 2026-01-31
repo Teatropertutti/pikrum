872921bd3e8446ea0dc730d455a9be8a871d2cfd
Teatropertutti <75387501+Teatropertutti@users.noreply.github.com>
2026-01-31 17:44:18 +0000

    bug fix

--- pikrum/backend/urls.py
+++ pikrum/backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.手admin.site.urls),
+    path('admin/', admin.site.admin.site.urls),
     # Questo collega il file urls.py della tua app
     path('api/', include('pikrum.api.urls')), 
 ]
