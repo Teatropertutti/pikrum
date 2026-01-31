3926700284ddfe61e1f8b5d272b1d5fe7a643d0e
Teatropertutti <75387501+Teatropertutti@users.noreply.github.com>
2026-01-31 17:42:04 +0000

    urls

--- pikrum/backend/urls.py
+++ pikrum/backend/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings # Importa i settings
-from django.conf.urls.static import static # Importa la funzione per i file statici
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('pikrum.api.urls')),
+    path('admin/', admin.site.手admin.site.urls),
+    # Questo collega il file urls.py della tua app
+    path('api/', include('pikrum.api.urls')), 
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
